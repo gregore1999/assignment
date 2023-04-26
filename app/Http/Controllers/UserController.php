844c61f5b28232942ea57f7a564974d8b0356247
gregore1999 <gregory_fasoulas@protonmail.com>
2023-04-26 12:57:24 +0300

    made calls for events and infos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,28 +4,83 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function index(Request $request)
+    {
+        if (!$request->user()) {
+            abort(401, 'Unauthorized');
+        }
+    }
+
     public function login(Request $request)
     {
-        $fields = $request->validate([
-            'username' => 'required|string',
-            'password' => 'required|string'
-        ]);
+        $credentials = $request->only('email', 'password');
      
-        
-    
-        if(auth()->attempt(request()->only(['username','password']))){
-            $user = User::where('username','=',$fields['username'])->first()->get();
-            $token = $user[0]->createToken('myAppToken')->plainTextToken;
-            session(['bearer' => $token]);
-           return  redirect()->route('/');
-            }
-           return back()->withErrors(
-            [
-                'invalid' => 'Username and password doesn\'t match any user'
-            ]
-        );
+        if (Auth::attempt($credentials)) {
+            
+            $user = Auth::user();
+            $token = $user->createToken('MyApp')->plainTextToken;
+            auth()->login($user);
+            return view('dashboard')->with('token', $token);
+        }
+       
+        return redirect()->route('login')->with('error', 'Invalid email or password');
     }
+    public function test(Request $request){
+        return 'ohhhh yeah';
+    }
+    public function register(Request $request)
+    {   
+        // $this->validate($request, [
+        //     'name' => 'required|string|max:255',
+        //     'email' => 'required|string|email|max:255|unique:users',
+        //     'password' => 'required|string|min:8',
+        // ]);
+        // return 'test';
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        
+
+
+        return "success";
+    }
+
+
+  public function getEvents(Request $request)
+    {    
+      $user = Auth::user();
+      return $user->events;
+    }
+
+
+    public function getAllInformation(Request $request)
+    {    
+      $user = Auth::user();
+      $events = $user->events;
+      
+    foreach( $events as $event){
+        $response['data']['events'][]= $event;
+      foreach ($event->topics as $topic) {
+        $response['data']['events']['topics']= $topic;
+    }
+    
+    foreach ($event->lessons as $lesson) {
+        $response['data']['events']['topics']= $lesson;
+    }
+    
+    foreach ($event->instructors as $instructor) {
+        $response['data']['events']['topics']= $instructor;
+    }
+  
+    }
+    $response['message'] = 'OK';
+    return response($response,200);
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\companiesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/test',[UserController::class,'test'])->name('test');
+    Route::get('/companies',[companiesController::class,'getCompanies'])->name('getCompanies');
+    Route::get('/events',[UserController::class,'getEvents'])->name('getEvents');
+    Route::get('/events/allInfo',[UserController::class,'getAllInformation'])->name('allInfo');
+});
+
+
+Route::post('/login',[UserController::class,'login'])->name('loginFun');
+Route::post('/register',[UserController::class,'register'])->name('register');
